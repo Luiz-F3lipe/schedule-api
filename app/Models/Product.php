8b715d46d8b1936fa8b08259e78c0eadf0daf139
LuizF3lipe <luizf3lipe.dev@gmail.com>
2026-03-25 15:50:38 -0300

    add relashionships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,9 +6,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+
+    public function system(): BelongsTo
+    {
+        return $this->belongsTo(System::class);
+    }
 }
--- app/Models/System.php
+++ app/Models/System.php
@@ -6,9 +6,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class System extends Model
 {
     /** @use HasFactory<\Database\Factories\SystemFactory> */
     use HasFactory;
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
