39b14223358e989662e0256060b82f2e7d2f767a
LuizF3lipe <luizf3lipe.dev@gmail.com>
2026-03-26 14:55:03 -0300

    fix configuration routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function () {
-            Route::group([], base_path('routes/api.php'));
+            Route::middleware('api')
+                ->group(base_path('routes/api.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
